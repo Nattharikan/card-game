490e9af7ee7c2dc2efe7485122aeb51882bd3c67
Nattharikan <5910110104@psu.ac.th>
2018-09-21 17:34:37 +0700

    ตอนที่ 7 _ เพิ่มเกมส์

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import WordCard from './WordCard';
+import NewGames from './NewGames';
  const word = "Hello";
+ const Noona = "PSU";
 class App extends Component {
   render() {
     return (
     <div className="activecard">
     <WordCard value={word}/>
+    <NewGames value={Noona}/>
     </div>
     );
    }
