9c0ad41a208f7c825f2bc80a295eabe5028f9cae
Nattharikan <5910110104@psu.ac.th>
2018-09-21 12:48:45 +0700

    ตอนที่ 5-2 Handler as Props

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
- export default class CharacterCard extends Component {
+ 
+export default class CharacterCard extends Component {
     constructor(props){
     super(props)
     this.state = {
-    active: false,
-    }
-    }
+   active: false,
+   }
+   }
+  
+   activate = () => {
    
-    activate = () => {
-    if(!this.state.active){
-    this.setState({active: true})
-    }
-    }
-    render(){
-    let className = `card ${this.state.active ? 'activeCard': ''}`
-    return (
+       if(!this.state.active){
+       this.props.activationHandler(this.props.value)
+       this.setState({active: true})
+       }
+      }
+  
+   
+   render(){
+   let className = `card ${this.state.active ? 'activeCard': ''}`
+   return (
     <div className={className} onClick={this.activate}>
     {this.props.value}
     </div>
     )
-   }    
+    }
    }
     
